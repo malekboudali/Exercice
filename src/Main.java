7a25eb4138bbaa6b2c4cdf2aef829f59cd827456
Malek Boudali <boudeli123@gmail.com>
2024-05-01 17:55:43 +0200

    Secondcomit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
 
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565");
         Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084");
+        Book book3 = new Book("To Kill", "Malek", "978-0061120088668");
 
         Library library = new Library(5);
 
 
         library.addBook(book1);
-        library.addBook(book2);
+        library.addBook(book3);
 
 
 
@@ -16,7 +17,7 @@ public class Main {
         library.showLibraryBooks();
 
 
-        boolean sameIsbn = Book.compareBooks(book1, book2);
+        boolean sameIsbn = Book.compareBooks(book1, book3);
         System.out.println("Are the ISBNs the same? " + sameIsbn);
     }
 }
